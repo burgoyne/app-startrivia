7a44abc0485999c9e28f589aac6cd36fef609a7e
Andre <aburgoyne@css.edu>
2018-09-14 11:15:11 -0600

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Andre Burgoyne on 2018-09-14.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
