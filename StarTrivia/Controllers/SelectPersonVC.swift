78589f27cd1064a0130f5ba74970a4b60ac21741
Andre <aburgoyne@css.edu>
2018-09-20 12:59:07 -0600

    able to parse data for main view

--- StarTrivia/Controllers/SelectPersonVC.swift
+++ StarTrivia/Controllers/SelectPersonVC.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class SelectPersonVC: UIViewController {
     
+    //outlets
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var massLbl: UILabel!
+    @IBOutlet weak var hairLbl: UILabel!
+    @IBOutlet weak var birthYearLbl: UILabel!
+    @IBOutlet weak var genderLbl: UILabel!
+    
+    
     //variables
     var personApi = PersonApi()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        personApi.getRandomPersonUrlSession()
     }
+    @IBAction func randomClicked(_ sender: Any) {
+        let random = Int.random(in: 1 ... 87)
+        
+        personApi.getRandomPersonUrlSession(id: random) { (person) in
+            if let person = person {
+                self.nameLbl.text = person.name
+                self.heightLbl.text = person.height
+                self.massLbl.text = person.mass
+                self.hairLbl.text = person.hair
+                self.birthYearLbl.text = person.birthYear
+                self.genderLbl.text = person.gender
+            }
+        }
+    }    
 }
 
--- StarTrivia/Services/PersonApi.swift
+++ StarTrivia/Services/PersonApi.swift
@@ -10,13 +10,14 @@ import Foundation
 
 class PersonApi {
     
-    func getRandomPersonUrlSession() {
+    func getRandomPersonUrlSession(id: Int, completion: @escaping PersonResponseCompletion) {
         
-        guard let url = URL(string: PERSON_URL) else { return }
+        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             
             guard error == nil else {
                 debugPrint(error.debugDescription)
+                completion(nil)
                 return
             }
             
@@ -25,12 +26,32 @@ class PersonApi {
             do {
                 let jsonAny = try JSONSerialization.jsonObject(with: data, options: [])
                 guard let json = jsonAny as? [String: Any] else { return }
-                print(json)
+                let person = self.parsePersonManual(json: json)
+                DispatchQueue.main.async {
+                    completion(person)
+                }
             } catch {
                 debugPrint(error.localizedDescription)
                 return
             }
         }
         task.resume()
+    }
+    
+    private func parsePersonManual(json: [String: Any]) -> Person {
+        let name = json["name"] as? String ?? "" //pull name as string, default to an empty string
+        let height = json["height"] as? String ?? ""
+        let mass = json["mass"] as? String ?? ""
+        let hair = json["hair_color"] as? String ?? ""
+        let birthYear = json["birth_year"] as? String ?? ""
+        let gender = json["gender"] as? String ?? ""
+        let homeworldUrl = json["homeworld"] as? String ?? ""
+        let filmUrls = json["films"] as? [String] ?? [String]()
+        let vehicleUrls = json["vehicles"] as? [String] ?? [String]()
+        let starshipUrls = json["starships"] as? [String] ?? [String]()
+        
+        let person = Person(name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeworldUrls: homeworldUrl, filmUrls: filmUrls, vehicleUrls: vehicleUrls, starshipUrls: starshipUrls)
+        
+        return person
     }
 }
